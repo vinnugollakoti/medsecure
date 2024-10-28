30443f33011d154f16d34e70003be2ca381481b6
Vinnu Gollakoti <vinnugollakoti@Vinnus-MacBook-Pro.local>
2024-10-28 09:40:39 +0530

    added

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 // CORS configuration
 app.use(cors({
-  origin: ["http://localhost:5173", "https://s60-vinay-blogie.onrender.com", "https://blogie-1.netlify.app", "http://localhost:3001"],
+  origin: ["http://localhost:5173", "https://s60-vinay-blogie.onrender.com", "https://blogie-1.netlify.app", "http://localhost:3001", "https://medsecure.netlify.app/"],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
